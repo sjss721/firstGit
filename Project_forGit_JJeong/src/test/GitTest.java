fade9e47c8d40391339ce705f840e05e9c583b00
David <David@jeongsik>
2015-08-27 09:35:42 +0900

    아침작업3

--- Project_forGit_JJeong/src/test/GitTest.java
+++ Project_forGit_JJeong/src/test/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("hi there~~!!!!");
+		System.out.println("SEOJEONGSIK");
 	}
 
 }
